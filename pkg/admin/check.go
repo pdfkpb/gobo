250283d90d012a22768ef2170d9411b3877997fd
Kevin Barnett <pdfkpb@gmail.com>
2022-08-14 12:33:26 -0700

    reformat add error help

--- pkg/admin/check.go
+++ pkg/admin/check.go
@@ -1,11 +1,11 @@
 package admin
 
 import (
-	"fmt"
+    "fmt"
 
-	"github.com/bwmarrin/discordgo"
+    "github.com/bwmarrin/discordgo"
 )
 
 func Check(bank map[string]int, params []string, s *discordgo.Session, m *discordgo.MessageCreate) {
-	s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("<@%s> has %d monies", m.Author.ID, bank[m.Author.ID]))
+    s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("<@%s> has %d monies", m.Author.ID, bank[m.Author.ID]))
 }
