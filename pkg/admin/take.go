cead4e65fa0fc85bee773fdb7d2bd6f75e26a362
pdfkpb <pdfkpb@gmail.com>
2022-09-11 11:42:22 -0700

    convert to new bank

--- pkg/admin/take.go
+++ pkg/admin/take.go
@@ -1,40 +1,64 @@
 package admin
 
 import (
-    "fmt"
-    "regexp"
-    "strconv"
+	"fmt"
+	"regexp"
+	"strconv"
 
-    "github.com/bwmarrin/discordgo"
+	"github.com/bwmarrin/discordgo"
+	"github.com/gobo/pkg/bank"
 )
 
-func Take(bank map[string]int, params []string, s *discordgo.Session, m *discordgo.MessageCreate) {
-    if (m.Author.ID == "303750733700923392" || m.Author.ID == "384902507383619594") && len(params) == 2 {
-        if match, err := regexp.Match("<@[0-9]{18}>", []byte(params[0])); match && err == nil {
-            uid := params[0][2:20]
-            giveTo, err := s.User(uid)
-            if err != nil || giveTo == nil {
-                s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Hmm not sure what fucked up there 'twas probably %s", uid))
-                return
-            }
+const (
+	HelpTake = "!take @SomeUser <some_amount>"
+)
 
-            amt, err := strconv.Atoi(params[1])
-            if err != nil {
-                s.ChannelMessageSend(m.ChannelID, "Somehow failed to parse the monies")
-                return
-            }
+func Take(params []string, s *discordgo.Session, m *discordgo.MessageCreate) {
+	if !ChatPox.IsAdmin(m.Author.ID) {
+		fmt.Printf("user %s tried to take funds: %v\n", m.Author.Username, ErrNotAdmin)
+		s.ChannelMessageSend(m.ChannelID, "Hey, knock it off")
+		return
+	}
 
-            if amt < 0 {
-                s.ChannelMessageSend(m.ChannelID, "Cannot take negative funds, must give positive")
-                return
-            }
+	if len(params) != 2 {
+		s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Usage: %s", HelpTake))
+		return
+	}
 
-            bank[uid] -= amt
-            s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("%s has %d monies", params[0], bank[uid]))
-        } else {
-            s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Fuck your regex and %s's eyebrows", params[0]))
-        }
-    } else {
-        s.ChannelMessageSend(m.ChannelID, "Only I Gobo... or Dylan/Kevin may take funds")
-    }
+	match, err := regexp.Match("<@[0-9]{18}>", []byte(params[0]))
+	if !match || err != nil {
+		s.ChannelMessageSend(m.ChannelID, fmt.Sprint("Not a user id"))
+		s.ChannelMessageSend(m.ChannelID, fmt.Sprint("  Usage: %s", HelpTake))
+		return
+	}
+
+	uid := params[0][2:20]
+	giveTo, err := s.User(uid)
+	if err != nil || giveTo == nil {
+		s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("User %s not found in this channel", params[0]))
+		return
+	}
+
+	amount, err := strconv.Atoi(params[1])
+	if err != nil {
+		s.ChannelMessageSend(m.ChannelID, fmt.Sprint("Not a valid monies amount"))
+		s.ChannelMessageSend(m.ChannelID, fmt.Sprint("  Usage: %s", HelpTake))
+		return
+	}
+
+	bankDB, err := bank.LoadBankDB()
+	if err != nil {
+		fmt.Printf("failed to load BankDB: %v\n", err)
+		s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Some backend error occured <@384902507383619594> fix it"))
+		return
+	}
+
+	currentFunds, err := bankDB.AddFunds(uid, amount)
+	if err != nil {
+		fmt.Printf("failed to add funds: %v\n", err)
+		s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Some backend error occured <@384902507383619594> fix it"))
+		return
+	}
+
+	s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("%s has %d monies", params[0], currentFunds))
 }
