2dc7e22f3c7aa7e27ad669b1caa33c314f86f0c3
pdfkpb <pdfkpb@gmail.com>
2022-09-11 10:37:00 -0700

    initial bank implementation

--- go.mod
+++ go.mod
@@ -2,10 +2,17 @@ module github.com/gobo
 
 go 1.18
 
-require github.com/bwmarrin/discordgo v0.25.0
+require (
+	github.com/bwmarrin/discordgo v0.25.0
+	gorm.io/driver/sqlite v1.3.6
+	gorm.io/gorm v1.23.8
+)
 
 require (
 	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/mattn/go-sqlite3 v1.14.15 // indirect
 	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
 	golang.org/x/sys v0.0.0-20201119102817-f84b799fce68 // indirect
 )
---
+++ pkg/bank/bank.go
@@ -0,0 +1,76 @@
+package bank
+
+import (
+	"errors"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+type BankDB struct {
+	db *gorm.DB
+}
+
+type Bank struct {
+	gorm.Model
+	userID int `gorm:"primarykey"`
+	funds  int
+}
+
+func LoadBankDB() (*BankDB, error) {
+	db, err := gorm.Open(sqlite.Open("bank.db"), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	db.AutoMigrate(&Bank{})
+
+	return &BankDB{
+		db: db,
+	}, nil
+}
+
+func (bdb *BankDB) AddUser(userID int) {
+	bdb.db.Create(&Bank{
+		userID: userID,
+		funds:  0,
+	})
+}
+
+func (bdb *BankDB) AddFunds(userID int, amount int) error {
+	if amount < 0 {
+		return errors.New("")
+	}
+
+	var bank Bank
+	result := bdb.db.First(&bank, userID)
+
+	if result.Error != nil {
+		return result.Error
+	}
+
+	bank.funds += amount
+
+	return bdb.db.Save(bank).Error
+}
+
+func (bdb *BankDB) TakeFunds(userID int, amount int) error {
+	if amount < 0 {
+		return errors.New("")
+	}
+
+	var bank Bank
+	result := bdb.db.First(&bank, userID)
+
+	if result.Error != nil {
+		return result.Error
+	}
+
+	if bank.funds-amount < 0 {
+		return errors.New("funds cannot be negative")
+	}
+
+	bank.funds -= amount
+
+	return bdb.db.Save(bank).Error
+}
