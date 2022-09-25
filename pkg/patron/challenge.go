30303a88ae8aa02c689b6596f8e037fe8d053b77
Kevin Barnett <pdfkpb@gmail.com>
2022-09-25 09:49:37 -0700

    fix challenge constraint

--- pkg/patron/challenge.go
+++ pkg/patron/challenge.go
@@ -5,8 +5,8 @@ import "gorm.io/gorm"
 type Challenge struct {
 	gorm.Model
 
-	SomethingElse int    `gorm:"primaryKey,autoIncrement"`
-	Contender     string `gorm:"unique"`
+	SomethingElse int `gorm:"primaryKey,autoIncrement"`
+	Contender     string
 
 	Escrow int
 }
